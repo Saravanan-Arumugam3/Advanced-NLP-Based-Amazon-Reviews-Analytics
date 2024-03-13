dc1cef889dbc1b53c68c0a40a2ea13d5aa9b9670
ruchirkanthg@gmail.com <ruchirkanthg@gmail.com>
2024-03-13 00:20:06 -0400

    Updated Formatter.py to work with dag

--- src/Formatter.py
+++ src/Formatter.py
@@ -11,54 +11,58 @@ import string
 from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+def tfidf_formatter():
+  AIRFLOW_HOME = os.environ.get('AIRFLOW_HOME', '/Users/phanibhavanaatluri')
+  current_directory = os.path.join(AIRFLOW_HOME, 'dags')
 
-current_directory = os.getcwd()
+  file_path = os.path.join(current_directory, 'Clean_1.csv')
 
-file_path = os.path.join(current_directory, 'Clean_1.csv')
+  def replace_text(dataframe, column, replace, substitute):
+    dataframe[column] = dataframe['reviewTime'].str.replace(replace, substitute)
+    return dataframe
 
-def replace_text(dataframe, column, replace, substitute):
-  dataframe[column] = dataframe['reviewTime'].str.replace(replace, substitute)
-  return dataframe
+  def delete_columns(dataframe, columns):
+    dataframe = dataframe.drop(columns, axis=1)
+    dataframe = dataframe.drop_duplicates()
+    dataframe = dataframe.dropna()
+    return dataframe
 
-def delete_columns(dataframe, columns):
-  dataframe = dataframe.drop(columns, axis=1)
-  dataframe = dataframe.drop_duplicates()
-  dataframe = dataframe.dropna()
-  return dataframe
+  nltk.download('stopwords')
+  def remove_punctuation(text):
+      return text.translate(str.maketrans('', '', string.punctuation))
 
-nltk.download('stopwords')
-def remove_punctuation(text):
-    return text.translate(str.maketrans('', '', string.punctuation))
+  stopwords_set = set(stopwords.words('english'))
+  def remove_stopwords(text):
+      return ' '.join(word for word in text.split() if word.lower() not in stopwords_set)
 
-stopwords_set = set(stopwords.words('english'))
-def remove_stopwords(text):
-    return ' '.join(word for word in text.split() if word.lower() not in stopwords_set)
 
-df = pd.read_csv(file_path)
 
-df = replace_text(df, 'reviewTime', ',', '')
-df = replace_text(df, 'reviewTime', ' ', '-')
-df['reviewTime'] = pd.to_datetime(df['reviewTime'])
+  df = pd.read_csv(file_path)
 
-df['reviews'] = df['reviewText']+ " " + df['summary']
-df = delete_columns(df, ['reviewText', 'summary'])
+  df = replace_text(df, 'reviewTime', ',', '')
+  df = replace_text(df, 'reviewTime', ' ', '-')
+  df['reviewTime'] = pd.to_datetime(df['reviewTime'])
 
-df['sentiment'] = np.where(df['overall'] > 3, 'Positive', np.where(df['overall'] < 3, 'Negative', 'Neutral'))
+  df['reviews'] = df['reviewText']+ " " + df['summary']
+  df = delete_columns(df, ['reviewText', 'summary'])
 
-nltk.download('stopwords')
-stopwords_set = stopwords.words('english')
+  df['sentiment'] = np.where(df['overall'] > 3, 'Positive', np.where(df['overall'] < 3, 'Negative', 'Neutral'))
 
-df['reviews'] = df['reviews'].str.replace('[^\w\s]','')
-pat = r'\b(?:{})\b'.format('|'.join(stopwords_set))
-df['reviews']  = df['reviews'] .str.replace(pat, '')
-df['reviews'] = df['reviews'] .str.replace(r'\s+', ' ')
+  nltk.download('stopwords')
+  stopwords_set = stopwords.words('english')
 
-tfidf_vectorizer = TfidfVectorizer()
-tfidf_matrix = tfidf_vectorizer.fit_transform(df['reviews'])
-tfidf_df = pd.DataFrame(tfidf_matrix.toarray(), columns=tfidf_vectorizer.get_feature_names_out())
+  df['reviews'] = df['reviews'].str.replace('[^\w\s]','')
+  pat = r'\b(?:{})\b'.format('|'.join(stopwords_set))
+  df['reviews']  = df['reviews'] .str.replace(pat, '')
+  df['reviews'] = df['reviews'] .str.replace(r'\s+', ' ')
 
-print(tfidf_df)
+  tfidf_vectorizer = TfidfVectorizer()
+  tfidf_matrix = tfidf_vectorizer.fit_transform(df['reviews'])
+  tfidf_df = pd.DataFrame(tfidf_matrix.toarray(), columns=tfidf_vectorizer.get_feature_names_out())
 
-tfidf_df.to_csv("tfidf.csv")
+  print(tfidf_df)
 
-os.remove("Clean_1.csv")
+  tfidf_csv_file = os.path.join(current_directory, 'tfidf_data.csv')
+  tfidf_df.to_csv(tfidf_csv_file, index=False)
+
+  os.remove("Clean_1.csv")
